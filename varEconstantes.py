46164bc0844f12d73172db5488fb201d725488ee
Marcos Vinicius <mvsenko7@gmail.com>
2026-05-05 14:36:58 -0300

    variaveis e constantes em python

---
+++ varEconstantes.py
@@ -0,0 +1,10 @@
+nome = "Marcos Vinicius"
+idade = 22
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+
+BRAZILIAN_STATES = ["SP", "RJ", "MG", "ES", "RS", "SC", "PR", "BA", "PE", "CE"]
+print(BRAZILIAN_STATES)
